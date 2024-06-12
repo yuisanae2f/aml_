8591f004661de0c73344e6f6e27dfc010b420db1
Yui San ae2f <84163330+yuisanae2f@users.noreply.github.com>
2024-06-13 01:43:42 +0900

    Create cItemChanges.java

---
+++ src/main/java/yuisanae2f/aml/ev/cItemChanges.java
@@ -0,0 +1,24 @@
+package yuisanae2f.aml.ev;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDropItemEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryEvent;
+
+public class cItemChanges implements Listener {
+    @EventHandler
+    public void ePick(EntityPickupItemEvent e) {
+
+    }
+
+    @EventHandler
+    public void eDrop(EntityDropItemEvent e) {
+
+    }
+
+    @EventHandler
+    public void pInv(InventoryEvent e) {
+        
+    }
+}
